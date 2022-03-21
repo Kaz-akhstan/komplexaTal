8b239cf739bca7e68843b217c99e6ba783c8465d
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-03-22 00:34:53 +0100

    Initial commit

---
+++ src/Komplex.java
@@ -0,0 +1,19 @@
+public class Komplex {
+    public Komplex(double t1, double t2) {
+        double r = 0;
+        double im = 0;
+    }
+
+    public Komplex add(Komplex b) {
+
+    }
+
+    public Komplex multiply(Komplex b) {
+
+    }
+
+    public double arg() {
+
+    }
+}
+
